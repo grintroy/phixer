581402371ea21eb24aa0c7366f74d4b8155d4ac8
grintroy <70978482+grintroy@users.noreply.github.com>
2022-04-13 22:02:45 +0100

    Implemented the customised sliders

--- script.js
+++ script.js
@@ -21,11 +21,6 @@
   }
 }
 
-// var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
-// var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-//   return new bootstrap.Dropdown(dropdownToggleEl)
-// })
-
 const phixer = new Phixer()
 
 function setup() {
@@ -47,10 +42,6 @@ function keyPressed() {
   }
 }
 
-function button() {
-
-}
-
 let playing = false
 
 const playButton = document.getElementById("play-button-img")
@@ -70,3 +61,58 @@ document.getElementById("player-button").onclick = () => {
     playing = false
   }
 }
+
+const rangesTextMatrix = {
+  "range-2-2": {
+    0: "widest",
+    20: "wide",
+    40: "optimal",
+    60: "narrow",
+    80: "narrowest"
+  },
+  "range-2-3": {
+    0: "lowest",
+    20: "low",
+    40: "optimal",
+    60: "high",
+    80: "highest"
+  }
+}
+
+const allRanges = document.querySelectorAll(".range-wrap")
+
+allRanges.forEach(wrap => {
+  const range = wrap.querySelector(".form-range")
+  const bubble = wrap.querySelector(".bubble")
+  setBubble(range, bubble)
+
+  range.addEventListener("input", () => {
+    range.addEventListener("input", () => {
+      setBubble(range, bubble)
+    })
+    setBubble(range, bubble)
+  })
+  setBubble(range, bubble)
+
+})
+
+function setBubble(range, bubble) {
+
+  const halfWidth = bubble.getBoundingClientRect().width / 2 // in px
+
+  let val = range.value
+
+  for (const levels of Object.keys(rangesTextMatrix[range.name])) {
+    if (Number(val) >= Number(levels)) {
+      bubble.innerHTML = rangesTextMatrix[range.name][levels]
+    }
+  }
+
+  const min = range.min
+  const max = range.max
+
+
+  bubble.style.left = `calc(${val}% - ${(val / 50 - 1) * 3}rem - ${(val / 50 - 1) * 1}rem - ${halfWidth}px)`
+}
+
+//
